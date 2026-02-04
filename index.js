a6d2ace1f8d2ee3b6303a1b7c0bf537fe06f62ba
rukundowilson <rukundowilson5@gmail.com>
2026-02-04 16:51:24 +0200

    initial

---
+++ index.js
@@ -0,0 +1,22 @@
+// src/index.ts
+import express from "express";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+dotenv.config(); // Load environment variables from .env file
+const app = express();
+const PORT = process.env.PORT || 5000;
+// Middleware
+app.use(bodyParser.json());
+// Routes
+app.get("/", (req, res) => {
+    res.send("EMS Backend is running!");
+});
+// Example API route
+app.get("/api/health", (req, res) => {
+    res.json({ status: "ok", timestamp: new Date() });
+});
+// Start servernpm 
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+//# sourceMappingURL=index.js.map
